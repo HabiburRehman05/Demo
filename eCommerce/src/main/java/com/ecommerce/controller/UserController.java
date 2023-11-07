fbcb9508ce97c21fe516a15b797e55d43850819e
Habibur Rehman Mansuri <hhabibman534@gmail.com>
2023-11-07 13:52:38 +0530

    first_Demo_Commit

---
+++ eCommerce/src/main/java/com/ecommerce/controller/UserController.java
@@ -0,0 +1,42 @@
+
+package com.ecommerce.controller;
+
+import com.ecommerce.models.User;
+import com.ecommerce.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getall")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/get/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("/add")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/update/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
